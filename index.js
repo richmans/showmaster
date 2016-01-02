da6ab87ca6d89f522b8724c543b62011e2ae7f42
Richard Bronkhorst <richardbronkhorst@hotmail.com>
2016-01-02 20:50:49 +0100

    input-output naming standardized, syncing output dials when reloading and when multiple clients use it

--- index.js
+++ index.js
@@ -164,12 +164,14 @@ function SockioDevice(showmaster) {
 
 	this.io.on('connection', function(socket){
 	  console.log('a user connected');
+    this.sendInitialValues();
 	  socket.on('disconnect', function(){
 	     console.log('user disconnected');
 	   });
 		 
-		socket.on("input", function(message) {
+		socket.on("output", function(message) {
       console.log(" << " , message);
+      socket.broadcast.emit("output", {"port": message["port"], "value": message["value"]});
       outputPort = this.outputPorts[message['port']]
       if (outputPort != undefined) {
         outputPort.update( message["value"]);
@@ -186,6 +188,14 @@ function SockioDevice(showmaster) {
 
 util.inherits(SockioDevice, Device);
 
+SockioDevice.prototype.sendInitialValues = function() {
+  for (var key in this.outputPorts) {
+    this.sendOutputValue(key, this.outputPorts[key].value);
+  }
+  for (var key in this.inputPorts) {
+    this.newValue(key, this.inputPorts[key].lastValue);
+  }
+}
 // this sets up some connections to a mixer device for testing
 // In the future, these ports are created and connected by api calls
 SockioDevice.prototype.setupTestConnections = function() {
@@ -203,13 +213,23 @@ SockioDevice.prototype.setupTestConnections = function() {
   this.connectFrom("input1", 0, "output");
 }
 
-SockioDevice.prototype.newValue = function(portName, value) {
+
+SockioDevice.prototype.sendOutputValue = function(portName, value) {
   var message = {
       "port": portName, 
       "value": value
     };
   console.log(" >> " , message);
  	this.io.emit("output", message);
+}
+
+SockioDevice.prototype.newValue = function(portName, value) {
+  var message = {
+      "port": portName, 
+      "value": value
+    };
+  console.log(" >> " , message);
+ 	this.io.emit("input", message);
 }
 
 
--- www/js/main.js
+++ www/js/main.js
@@ -5,13 +5,19 @@ $(function() {
 	$(".dial").knob({
 		'change': function(v) {
 			myname = this.i[0].id.split("-")[1];
-    	socket.emit("input", {"port": myname, "value": v / 100});
+    	socket.emit("output", {"port": myname, "value": v / 100});
 		}
 	});
   socket.on('output', function(message){
     portName = message["port"]
   	value = Math.round(message["value"] * 100);
   	$("#output-" + portName).val(value).trigger("change");
+  });
+  
+  socket.on('input', function(message){
+    portName = message["port"]
+  	value = Math.round(message["value"] * 100);
+  	$("#input-" + portName).val(value).trigger("change");
   });
 });
 
