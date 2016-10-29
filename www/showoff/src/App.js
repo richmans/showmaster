dcfc56c2dd23d21fe8eb44eb5f44514de6636c50
Richard Bronkhorst <richardbronkhorst@hotmail.com>
2016-10-29 23:00:38 +0200

    started adding interaction

--- www/showoff/src/App.js
+++ www/showoff/src/App.js
@@ -27,16 +27,42 @@ class Navbar extends Component {
 }
 
 class ShowView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      active_scene: 0
+    };
+  
+    this.handleSceneChange = this.handleSceneChange.bind(this);
+  }
+  
+  handleSceneChange(active_scene, other, things, and, such) {
+    console.log( active_scene)
+    console.log( other)
+    console.log( things)
+    console.log( and)
+    console.log( such)
+    this.setState({
+      active_scene: active_scene
+    });
+  }
+  
   render() {
-    var active_scene = 1;
     return (
     <div className="container">
       <div className="row">
         <div className="col-md-3 ">
           <div className="list-group schedule-scenes">
-            <ProgramView active={active_scene} scenes={this.props.schedule.schedule}/>
-            <QuickProgramPicker programs={this.props.schedule.programs}/>
+            <ProgramView 
+              active={this.state.active_scene} 
+              scenes={this.props.schedule.schedule}
+              onSceneChange={this.handleSceneChange}
+            />
           </div>
+         <QuickProgramPicker 
+            groups={this.props.schedule.groups} 
+            programs={this.props.schedule.programs}
+            />
         </div>
         <div className="col-md-3">
           <SceneView scene_lookup="next"/>
@@ -54,9 +80,15 @@ class ShowView extends Component {
 }
 
 class ProgramSceneView extends Component {
+  onSceneChange() {
+    console.log(this.props.sceneKey)
+    this.props.onSceneChange(this.props.sceneKey);
+  }
+  
   render() {
     return (
-      <a href="#" className={"list-group-item " + this.props.active}>
+      <a href="#" className={"list-group-item " + this.props.active}
+          onClick={this.onSceneChange.bind(this)}>
         <h4 className="list-group-item-heading">{this.props.name}</h4>
         <p className="list-group-item-text">{this.props.desc}</p>
       </a>
@@ -68,10 +100,17 @@ class ProgramView extends Component {
     var scenes = []
     
     for(var key in this.props.scenes) {
+      console.log(key)
       var scene = this.props.scenes[key];
       var active = (this.props.active == key)? "active": "";
       scenes.push(
-        <ProgramSceneView active={active} name={scene.name} desc={scene.desc}/>
+        <ProgramSceneView 
+           active={active} 
+           name={scene.name} 
+           desc={scene.desc}
+           sceneKey={key}
+           onSceneChange={this.props.onSceneChange}
+          />
       )
     }
     return (
@@ -86,7 +125,7 @@ class ProgramView extends Component {
 class QuickProgram extends Component {
   render() {
     return (
-      <span className="label label-danger">{this.props.name}</span>
+      <span className={"label label-" + this.props.color}>{this.props.name}</span>
     );
   }
 }
@@ -94,8 +133,12 @@ class QuickProgramPicker extends Component {
   render() {
     var programs = []
     for (var program_name in this.props.programs){
-      programs.push(<QuickProgram name={program_name}/>)
+      programs.push(<QuickProgram name={program_name} color='danger'/>)
     };
+    for (var program_name in this.props.groups){
+      programs.push(<QuickProgram name={program_name} color='success'/>)
+    };
+    
     return (
       <div className='quick-programs'>
         <h4>Quick Programs</h4>
